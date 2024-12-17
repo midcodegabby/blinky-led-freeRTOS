7eeda8c2d2ae6bb6b47554e763d63b3f40a40c8b
midcodegabby <gabrielrodgers4i@gmail.com>
2024-12-16 16:59:47 -0800

    populated repo with basic code

---
+++ main.c
@@ -0,0 +1,62 @@
+//	Author: Gabriel Rodgers
+//	Date: 8/1/2024
+//	Purpose: To get the LD2 on the Nucleo-L476RG to turn on. 
+
+#include <stdint.h>
+
+#define GPIOA 0x48000000
+#define RCC 0x40021000
+
+#define GPIOA_ODR (*((volatile uint32_t *) (GPIOA + 0x14)))
+#define GPIOA_MODER (*((volatile uint32_t *) GPIOA))
+#define GPIOA_OTYPER (*((volatile uint32_t *) (GPIOA + 0x04)))
+#define GPIOA_OSPEEDR (*((volatile uint32_t *) (GPIOA + 0x08)))
+#define GPIOA_PUPDR (*((volatile uint32_t *) (GPIOA + 0x0C)))
+#define GPIOA_BSRR (*((volatile uint32_t *) (GPIOA + 0x18)))
+
+//define the first register in the RCC memory section
+//this method directly dereferences the memory itself to access the registers
+#define RCC_CR (*((volatile uint32_t *) RCC)) 	//sysclk config
+#define RCC_CFGR (*((volatile uint32_t *) (RCC + 0x08)))	//clk config
+#define RCC_AHB1ENR (*((volatile uint32_t *) (RCC + 0x48)))	//clk enable for peripherals
+#define RCC_AHB2ENR (*((volatile uint32_t *) (RCC + 0x4C)))	//clk enable for peripherals
+#define RCC_APB1ENR1 (*((volatile uint32_t *) (RCC + 0x58)))	//clk enable for peripherals
+#define RCC_CCIPR (*((volatile uint32_t *) (RCC + 0x88)))		//clk config for peripherals
+
+
+void led_on(void){
+
+	//set LED pin (GPIOA pin 5) to be general purpose output mode
+	GPIOA_MODER |= (1 << 10);
+	GPIOA_MODER &= ~(1 << 11);
+
+	GPIOA_OTYPER &= ~(1 << 5); // set PA5 to be push-pull
+				   
+	GPIOA_ODR |= (1 << 5); // set led ON
+}
+
+int main(void) {
+	
+	//enable clk for peripherals (GPIO Port A)
+	RCC_AHB2ENR |= (1 << 0);	
+
+	for(uint32_t i=0; i < 2; i++); 
+
+	led_on();
+
+	uint32_t val = 0;
+
+	while (1) {
+	/*	
+		if (val == 100000) {
+
+			GPIOA_ODR &= ~(1 << 5); // set led ON
+			val = 0;
+		}
+
+		val++;
+	*/
+	}
+
+	return 0;
+}
---
+++ makefile
@@ -0,0 +1,33 @@
+# note for compilation: can call a specific function defined here by using
+# [make func_name] --> e.g., make clean
+CC=arm-none-eabi-gcc
+MACH=cortex-m4
+CFLAGS=-c -mcpu=$(MACH) -mthumb --std=gnu11 -Wall -o0 -g3
+LDFLAGS=-nostdlib -T stm32_ls.ld -Wl,-Map=final.map #-Wl specifies a linker input
+
+all: main.o stm32_startup.o final.elf
+
+main.o:main.c
+	$(CC) $(CFLAGS) -o $@ $^
+
+stm32_startup.o:stm32_startup.c
+	$(CC) $(CFLAGS) -o $@ $^
+
+final.elf: main.o stm32_startup.o
+	$(CC) $(LDFLAGS) -o $@ $^
+
+clean:
+	rm -rf *.o *.elf *.map
+
+load:
+	openocd -f interface/jlink.cfg -c "transport select hla_swd" -f target/stm32l4x.cfg
+
+client:
+	arm-none-eabi-gdb
+
+# -Wall: show all warnings
+#  clean: function description deletes all .o and .elf files
+#  -c: do not link the files
+#  -o: create object files
+#
+
