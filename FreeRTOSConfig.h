af49d5d1be690fbab2e210e9c491f2a83f8ac7ce
midcodegabby <gabrielrodgers4i@gmail.com>
2024-12-28 23:05:09 -0800

    removed first part in FreeRTOSConfig.h, which did some compilation stuff for IAR compiler, which I am not using. I also set the config_CPU_CLOCK_HZ value to 48000000U, which is the clock frequency. Next: configure main.c to create tasks correctly.

--- FreeRTOSConfig.h
+++ FreeRTOSConfig.h
@@ -40,15 +40,10 @@
  * See http://www.freertos.org/a00110.html
  *----------------------------------------------------------*/
 
-/* Ensure stdint is only used by the compiler, and not the assembler. */
-#ifdef __ICCARM__
-	#include <stdint.h>
-#endif
-
 #define configUSE_PREEMPTION			1
 #define configUSE_IDLE_HOOK				1
 #define configUSE_TICK_HOOK				1
-#define configCPU_CLOCK_HZ				( 4000000U )
+#define configCPU_CLOCK_HZ				( 48000000U )
 #define configTICK_RATE_HZ				( ( TickType_t ) 1000 )
 #define configMAX_PRIORITIES			( 5 )
 #define configMINIMAL_STACK_SIZE		( ( unsigned short ) 130 )
--- src/main.c
+++ src/main.c
@@ -19,36 +19,17 @@ int main(void) {
 	sysclk_init();
 	peripheral_clk_init();
 
-	nvic_enable();
+	//nvic_enable();
 	//nvic_priority();
 
-	gpio_led_init();
+	//gpio_led_init();
 	//gpio_led_on();
 
-	timer2_init();
-	timer2_enable();
+	//timer2_init();
+	//timer2_enable();
 
 	while (1) {
-		/*
-		if (timer2_check() == 1) {
-			//timer2_clear();
-			//gpio_led_toggle();
-		}
-		*/
-
-		/*
-		while (TIM2_CNT <= 0xFFFFF) {
-		}
-
-		gpio_led_toggle();
-		*/
-
-		/*
-		while ((TIM2_SR & 1) != 1) {
-		}
-		gpio_led_toggle();
-		TIM2_SR &= ~(1);
-		*/
+		
 	}
 
 	return 0;
